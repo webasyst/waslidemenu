3b1bb8db2bca1619188663bd640f9f0de74cd59c
Kirill Maramygin <kirill.maramygin@gmail.com>
2014-04-29 16:40:04 +0400

    - add underscore for 'private' methods; - new destroy method; - ability to call destroy method ('exec','detroy');

--- src/js/waslidemenu.js
+++ src/js/waslidemenu.js
@@ -1,5 +1,5 @@
 /*global $, jQuery*/
-(function ($, window, document) {
+(function ($, window, document, undefined) {
     "use strict";
 
     $.waSlideMenu = function (el, options) {
@@ -8,7 +8,7 @@
             element = el;
 
         // Apply namespace to css classes
-        base.addCssNamespace = function (attrNames, namespace) {
+        base._addCssNamespace = function (attrNames, namespace) {
             var obj = {};
             $.each(attrNames, function (k, v) {
                 if (typeof v === 'string') {
@@ -19,10 +19,10 @@
             return obj;
         };
         // setup DOM elements, variables
-        base.setup = function () {
+        base._setup = function () {
             base.o = $.extend({}, $.waSlideMenu.defaults, options);
 
-            base.o.classNames = base.addCssNamespace(base.o.classNames, base.o.namespace);
+            base.o.classNames = base._addCssNamespace(base.o.classNames, base.o.namespace);
 
             // Access to jQuery and DOM versions of element
             base.$waSlideMenu = $(el);
@@ -82,14 +82,14 @@
         // go to selected item
         base.gotoSelected = function (depth, animate) {
             // we need to go deeper
-            base.hideOtherMenus(depth, base.scrollToTop);
+            base._hideOtherMenus(depth, base._scrollToTop);
             // height fix
-            base.heightFix(depth);
+            base._heightFix(depth);
             // sliiiiide to other level
-            base.animateSlide(depth, animate);
+            base._animateSlide(depth, animate);
         };
         // animate function
-        base.animateSlide = function (depth, animate) {
+        base._animateSlide = function (depth, animate) {
             var amount = Math.abs(depth * 100),
                 callback_slide = depth > 0 ? base.o.onSlideForward : base.o.onSlideBack;
 
@@ -105,7 +105,7 @@
                     base.o.slideSpeed,
                     base.o.slideEasing,
                     function () {
-                        base.scrollToTop(depth);
+                        base._scrollToTop(depth);
                         // afterSlide callback
                         if (base.o.afterSlide && typeof (base.o.afterSlide) === 'function') {
                             base.o.afterSlide(base);
@@ -116,8 +116,7 @@
                 base.$movable.css('left', '-' + amount + '%');
             }
         };
-
-        base.scrollToTop = function (depth) {
+        base._scrollToTop = function (depth) {
             var $menu_children = base.$currentMenuElement.children(base.o.menuSelector),
                 back_link = base.$currentMenuElement.hasClass(base.o.classNames.menuItemBackClass) ? true : false;
 
@@ -134,8 +133,7 @@
                 }
             }
         };
-
-        base.heightFix = function (depth) {
+        base._heightFix = function (depth) {
             if (base.o.autoHeightMenu) {
                 var h = 0,
                     to_currentmenuelement_h = 0;
@@ -160,8 +158,7 @@
                 base.$waSlideMenu.css('height', h);
             }
         };
-
-        base.hideOtherMenus = function (depth) {
+        base._hideOtherMenus = function (depth) {
             var $branchParent = base.$currentMenuElement.parentsUntil('.' + base.o.classNames.navigationClass).filter(base.o.itemSelector);
 
             $branchParent.push(base.$currentMenuElement[0]);
@@ -173,7 +170,6 @@
                 $branchParent.children(base.o.menuSelector).css('visibility', 'visible');
             }
         };
-
         base.loadContent = function (loadContainer, url) {
             var loading = $('<span/>', {
                     'class' : base.o.classNames.loadingClass
@@ -222,68 +218,17 @@
                     base.$waSlideMenu.trigger('afterLoadAlways.' + base.o.namespace + pluginName);
                 });
         };
-
         base.changeTitle = function () {
             if (base.o.setTitle) {
                 $('title').text(base.$currentMenuElement.children('a').text());
             }
         };
-
         base.changeUri = function (url) {
             if (!!(window.history && history.pushState)) {
                 window.history.pushState({}, '', url);
             }
         };
-
-        base.init = function () {
-            base.setup();
-
-            $.each(base.$backs, function (index, back) {
-                back.on('click', 'a', function (e) {
-                    base.menuItemClicked(this, e, -1);
-                });
-            });
-            base.$items.on('click', 'a', function (e) {
-                base.menuItemClicked(this, e, 1);
-            });
-
-            if (!!(window.history && history.pushState)) {
-                window.onpopstate = function (event) {
-                    if (event && event.state) {
-                        location.reload();
-                    }
-                };
-            }
-
-            var $selectedItem = base.$waSlideMenu
-                .find('.' + base.o.selectedClass)
-                .first();
-
-            if ($selectedItem.length) {
-                base.$currentMenuElement = $selectedItem;
-                var $url = base.$currentMenuElement.children('a').first(),
-                    depth = $selectedItem.parentsUntil('.' + base.o.classNames.navigationClass).filter(base.o.itemSelector).length,
-                    $selectedItemChildrenLen = $selectedItem.children(base.o.menuSelector).length;
-                if ($.inArray($url.attr('href'), base.o.excludeUri) < 0) {
-                    // if we have menus inside current element
-                    if ($selectedItemChildrenLen > 0) {
-                        // slide to them
-                        depth += 1;
-                    }
-                    base.gotoSelected(depth, false);
-                    // heightFix hack if we show last menu
-                    base.heightFix($selectedItemChildrenLen);
-                }
-            }
-
-            // onInit callback
-            if (base.o.onInit && typeof (base.o.onInit) === 'function') {
-                base.o.onInit(base);
-            }
-            base.$waSlideMenu.trigger('onInit.' + base.o.namespace + pluginName);
-        };
-
-        base.menuItemClicked = function (element, event, depth) {
+        base._menuItemClicked = function (element, event, depth) {
             event.preventDefault();
             event.stopPropagation();
 
@@ -322,13 +267,84 @@
                 base.gotoSelected(depth, true);
             }
         };
+        base._menuDown = function (e) {
+            base._menuItemClicked(this, e, 1);
+        };
+        base._menuUp = function (e) {
+            base._menuItemClicked(this, e, -1);
+        };
+        base._remove = function () {
+            base.$items.off('click', 'a', base._menuDown);
+
+            base.$waSlideMenu.removeClass(base.o.classNames.navigationClass);
+            base.$rootMenu.removeClass(base.o.classNames.allMenusClass)
+                            .prependTo(base.$waSlideMenu);
+            base.$inheritedMenus.removeClass(base.o.classNames.allMenusClass + ' ' + base.o.classNames.inheritedMenuClass);
+            base.$inheritedMenus.css('visibility', '');
+            $.each(base.$backs, function (index, back) {
+                back.remove();
+            });
+            base.$movable.remove();
+            base.$waSlideMenu.removeData(pluginName);
+        };
+        base.exec = function(func) {
+            if (base.hasOwnProperty(func) && typeof base[func] === 'function') {
+                base[func].call();
+            }
+        };
+        base.destroy = function () {
+            base._remove();
+        };
+        base.init = function () {
+            base._setup();
+
+            $.each(base.$backs, function (index, back) {
+                back.on('click', 'a', base._menuUp);
+            });
+            base.$items.on('click', 'a', base._menuDown);
+
+            if (!!(window.history && history.pushState)) {
+                window.onpopstate = function (event) {
+                    if (event && event.state) {
+                        location.reload();
+                    }
+                };
+            }
+
+            var $selectedItem = base.$waSlideMenu
+                .find('.' + base.o.selectedClass)
+                .first();
+
+            if ($selectedItem.length) {
+                base.$currentMenuElement = $selectedItem;
+                var $url = base.$currentMenuElement.children('a').first(),
+                    depth = $selectedItem.parentsUntil('.' + base.o.classNames.navigationClass).filter(base.o.itemSelector).length,
+                    $selectedItemChildrenLen = $selectedItem.children(base.o.menuSelector).length;
+                if ($.inArray($url.attr('href'), base.o.excludeUri) < 0) {
+                    // if we have menus inside current element
+                    if ($selectedItemChildrenLen > 0) {
+                        // slide to them
+                        depth += 1;
+                    }
+                    base.gotoSelected(depth, false);
+                    // heightFix hack if we show last menu
+                    base._heightFix($selectedItemChildrenLen);
+                }
+            }
+
+            // onInit callback
+            if (base.o.onInit && typeof (base.o.onInit) === 'function') {
+                base.o.onInit(base);
+            }
+            base.$waSlideMenu.trigger('onInit.' + base.o.namespace + pluginName);
+        };
 
         var oldBase = $(el).data(pluginName);
         if (typeof oldBase === 'object') {
             $.each(options, function (k, v) {
                 if (typeof oldBase[k] === 'function') {
                     oldBase[k](v);
-                } else {
+                } else if (oldBase.o.hasOwnProperty(k)){
                     oldBase.o[k] = v;
                 }
             });
