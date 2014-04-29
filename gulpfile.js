ec67580abd2dccc2c7e71568579e9582991d9959
Kirill Maramygin <kirill.maramygin@gmail.com>
2014-04-29 16:35:27 +0400

    new gulp

--- gulpfile.js
+++ gulpfile.js
@@ -21,12 +21,13 @@ var banner = ['/**',
 gulp.task('wacss', function() {
     return gulp.src('./src/css/waslidemenu.css')
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(gulp.dest('./demo/css'))
         .pipe(rename({
             suffix: '.min'
         }))
         .pipe(minifycss())
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./dist'))
         .pipe(notify({
             message: 'CSS task complete'
         }));
@@ -40,39 +41,25 @@ gulp.task('wajs', function() {
         .pipe(header(banner, { pkg : pkg } ))
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'))
-		// .pipe(header('waSlideMenu\n')
-		.pipe(gulp.dest('./js'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(gulp.dest('./demo/js'))
 		.pipe(rename({
 			suffix: '.min'
 		}))
         .pipe(uglify())
 		.pipe(header(banner, { pkg : pkg } ))
-        .pipe(gulp.dest('./js'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(gulp.dest('./demo/js'))
 		.pipe(notify({
             message: 'JS task complete'
         }));
 });
 
-gulp.task('vendorjs', function() {
-    return gulp.src('./src/js/vendor/**/*.js')
-        .pipe(gulp.dest('./js/vendor'))
-        .pipe(notify({
-            message: 'Vendor JS task complete'
-        }));
-});
-gulp.task('vendorcss', function() {
-    return gulp.src('./src/css/vendor/**/*.css')
-        .pipe(gulp.dest('./css/vendor'))
-        .pipe(notify({
-            message: 'CSS task complete'
-        }));
-});
-
 gulp.task('clean', function() {
-  return gulp.src(['./css', './js'], {read: false})
+  return gulp.src([ './dist','./js','./css', './demo/css/waslidemenu*.css', './demo/js/*waslidemenu*.js'], {read: false})
     .pipe(clean());
 });
 
 gulp.task('default', ['clean'], function() {
-	gulp.start('wacss', 'wajs', 'vendorjs', 'vendorcss');
+	gulp.start('wacss', 'wajs');
 });
